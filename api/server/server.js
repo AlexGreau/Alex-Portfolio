6fa7c2e154f5beba5b5ad1faf7dd64eca5d2f24c
Alexandre Gréau <alexgreau@live.fr>
2020-05-02 01:13:57 +0200

    Debug route

--- api/server/server.js
+++ api/server/server.js
@@ -8,10 +8,15 @@ const port = process.env.PORT || 3001;
 
 app.use(express.static(buildPath));
 
-app.get('*', (req, res) => {
+app.get('/path', (req, res) => {
+  res.end("PATH TO build : " + buildPath);
+});
+
+app.get('*',(req, res) => { // site content
   res.sendFile(path.join(buildPath, 'index.html'));
-}); 
+});
 
 app.listen(port, () => {
+  console.log(buildPath);
   console.log('Server is up! visit on http://' + hostname + ':' + port + '/');
 });
