7741a98791e0fe79b7166fb370b908961d4a8cf3
Alexandre Gréau <alexgreau@live.fr>
2020-05-02 01:30:58 +0200

    Added build folder

---
+++ ui/build/precache-manifest.4bc4ff7ed6298ed4ef9e22629f988015.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e6e243878a88ae988fb4796cb8ed0fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a53b90db761c74e6079",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "004414516d2e3544612f",
+    "url": "/static/js/2.1a02a5aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a02a5aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a53b90db761c74e6079",
+    "url": "/static/js/main.11a6a01f.chunk.js"
+  },
+  {
+    "revision": "fbe5e9f300b54b9a5f91",
+    "url": "/static/js/runtime-main.d66048e2.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
