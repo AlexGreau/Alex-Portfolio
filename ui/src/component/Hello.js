1e13083fadeff82ffc62bb2b6bbbdf21f33a50e9
Alexandre Gréau <alexgreau@live.fr>
2020-06-02 01:29:16 +0200

    removed redundant alt

--- ui/src/component/Hello.js
+++ ui/src/component/Hello.js
@@ -6,7 +6,7 @@ import hello from '../assets/hello.jpg';
 const Hello = () => {
     return (
         <div id='helloDiv' className='p-0'>
-            <img id='hello' src={hello} className="img-fluid" alt="Responsive image"></img>
+            <img id='hello' src={hello} className="img-fluid" alt="bear Hello"></img>
         </div>
     )
 }
