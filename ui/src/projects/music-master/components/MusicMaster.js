162a9b58a2411590235faf64b1821a1b901fe191
Alexandre Gréau <alexgreau@live.fr>
2020-06-04 00:53:30 +0200

    artist query done

--- ui/src/projects/music-master/components/MusicMaster.js
+++ ui/src/projects/music-master/components/MusicMaster.js
@@ -2,18 +2,47 @@ import React, { Component } from 'react';
 import './components.css'
 
 class MusicMaster extends Component {
-    state = {artistQuery: ''};
+    state = { artistQuery: '', artistResult: {} };
 
-    updateArtistQuery = () => {
-
+    updateArtistQuery = (event) => {
+        console.log(event.target.value)
+        this.setState({ artistQuery: event.target.value })
     }
 
-    render () {
+    searchArtist = () => {
+        const url = 'https://spotify-api-wrapper.appspot.com/artist/' + this.state.artistQuery;
+        fetch(url)
+            .then((response) => response.json())
+            .then((res) => {
+                console.log(res)
+                this.setState({artistResult: res.artists.items[0]})
+                console.log(this.state)
+            })
+    }
+
+    handleKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            this.searchArtist();
+        }
+    }
+
+    render() {
         return (
-            <div id="body">
+            <div className="musicMaster">
                 <h2>Music Master</h2>
-                <input placeholder='Search for an Artist' onChange={this.updateArtistQuery}></input>
-                <button className="btn btn-dark">Search</button>
+                <div className="row justify-content-center">
+                    <input className="form-control mr-sm-2 col-2"
+                        onChange={this.updateArtistQuery}
+                        onKeyPress={this.handleKeyPress}
+                        placeholder="Search for an Artist" ></input>
+                    <button className="btn btn-outline-info my-2 my-sm-0"
+                        onClick={this.searchArtist}>
+                        Search
+                        </button>
+                </div>
+                <div>
+                    {this.state.artistQuery}
+                </div>
             </div>
         )
     }
