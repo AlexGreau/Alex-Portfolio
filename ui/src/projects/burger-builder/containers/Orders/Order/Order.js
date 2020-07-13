10681ac5f2ef77c1ab96c5a1c66ce6d71c380954
Alexandre Gréau <alexgreau@live.fr>
2020-07-13 21:13:04 +0200

    Styling for Orders' ingredients output

--- ui/src/projects/burger-builder/containers/Orders/Order/Order.js
+++ ui/src/projects/burger-builder/containers/Orders/Order/Order.js
@@ -2,25 +2,32 @@ import React, { useEffect, useState } from 'react';
 import classes from './Order.module.css';
 
 const Order = (props) => {
-    const [ingredients, setIngredients] = useState("");
+    const [ingredients, setIngredients] = useState([]);
 
     useEffect(() => {
         let ing = [];
         console.log(props.ingredients)
         for (let ingredient in props.ingredients) {
-            let str = "" + ingredient + " (" + props.ingredients[ingredient] + ")"
-            ing.push(str);
+            ing.push({
+                name: ingredient,
+                amount: props.ingredients[ingredient]
+            });
         }
-        const res = ing.join(", ");
-        setIngredients(res);
+        setIngredients(ing);
         return () => {
             return null;
         }
     }, [])
 
+    const ingredientsOutput = ingredients.map(
+        ig=>{
+            return <span key={ig.name} className={classes.ingredientList}>{ig.name} ({ig.amount}) </span>
+        }
+    ) 
+
     return (
         <div className={classes.Order}>
-            <p>Ingredients : {ingredients}</p>
+            <p>Ingredients : {ingredientsOutput}</p>
             <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
         </div>
     )
